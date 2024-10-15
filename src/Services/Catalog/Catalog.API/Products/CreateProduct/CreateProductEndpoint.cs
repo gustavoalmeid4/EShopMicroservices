56eb0b576dbe47443219abb97f87f45fa10e02f9
Gustavo Almeida <almades159@gmail.com>
2024-10-15 16:26:54 -0300

    criado enpoints de catalog.

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,7 +1,30 @@
 ﻿namespace Catalog.API.Products.CreateProduct
 {
-    public class CreateProductEndpoint
+    public record CreateProductRequest(Guid Id, string Name, string Description, string ImageFile, decimal Price, List<string> Category);
+    
+    public record CreateProductResponse(Guid id);
+    public class CreateProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products",
+                async (CreateProductResponse request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
 
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<CreateProductResponse>();
+
+                return Results.Created($"/products/{response.id}", response);
+
+            })
+                .WithName("Create product")
+                .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("CreateProduct")
+                .WithDescription("CreateProduct");
+
+        }
     }
 }
