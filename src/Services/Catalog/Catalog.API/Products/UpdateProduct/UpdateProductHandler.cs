7ba4b71a8860e827402d85e0936ef38b4216990f
Gustavo Almeida <almades159@gmail.com>
2024-10-29 22:11:13 -0300

    Criado update product

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -1,6 +1,40 @@
 ﻿namespace Catalog.API.Products.UpdateProduct
 {
-    public class UpdateProductHandler
+    public record UpdateProductCommand(
+        Guid Id,
+        string Name,
+        string Description,
+        string ImageFile,
+        decimal Price,
+        List<string> Category
+    ) : ICommand<UpdateProductResult>;
+
+    public record UpdateProductResult(bool IsSuccess);
+
+    internal class UpdateProductHandler
+    (IDocumentSession session, ILogger<UpdateProductHandler> logger)
+        : ICommandHandler<UpdateProductCommand, UpdateProductResult>
     {
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("UpdateProductHandler.Handle called with {@command}", command);
+            
+            var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+            if (product is null)
+                throw new ProductNotFoundException();
+            
+            product.Name = command.Name;
+            product.Description = command.Description;
+            product.ImageFile = command.ImageFile;
+            product.Price = command.Price;
+            product.Category = command.Category;
+            
+            session.Update(product);
+            await session.SaveChangesAsync(cancellationToken);
+            
+            return new UpdateProductResult(true); 
+            
+        }
     }
 }
